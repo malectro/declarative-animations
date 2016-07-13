91907250874d758c07c969b9702a819e4e147356
Kyle Warren <qarren@gmail.com>
2016-07-13 14:16:29 -0700

    can do repeated animations

--- timeline.js
+++ timeline.js
@@ -40,67 +40,93 @@ const elements = [
 ];
 
 
-const end = elements.reduce((end, element) => (
-  element.frames.reduce((end, {time, duration}) => (
-    Math.max(time + (duration || 0), end)
-  ), end)
-), 0);
-const compiledElements = elements.map(element => {
-  const frames = element.frames.map(frame => {
-    const duration = frame.duration || 0;
-    const end = frame.time + duration;
-    return Object.assign({}, frame, {
-      duration,
+function createAnimation(elements) {
+  elements = elements.map(element => {
+    const frames = element.frames.map(frame => {
+      const duration = frame.duration || 0;
+      const end = frame.time + duration;
+      return Object.assign({}, frame, {
+        duration,
+        end,
+      });
+    });
+
+    const start = frames.reduce((min, {time}) => Math.min(min, time), Infinity);
+    const end = frames.reduce((max, {end}) => Math.max(max, end), 0);
+
+    return Object.assign({}, element, {
+      frames,
+      start,
       end,
     });
   });
 
-  const start = frames.reduce((min, {time}) => Math.min(min, time), Infinity);
-  const end = frames.reduce((max, {end}) => Math.max(max, end), 0);
+  const duration = elements.reduce((end, element) => (
+    Math.max(end, element.end)
+  ), 0);
 
-  return Object.assign({}, element, {
-    frames,
-    start,
-    end,
-  });
-});
+  return {
+    elements,
+    duration,
+  };
+}
 
-console.log(compiledElements);
-
+const animation = createAnimation(elements);
 
 const svg = document.getElementById('timeline');
+let animations = new Set();
 
 let lastTime = 0;
+function animate(animation) {
+  animation = Object.assign({}, animation, {
+    start: lastTime,
+    end: lastTime + animation.duration,
+  });
+  animations.add(animation);
+}
+
 function update(time) {
   requestAnimationFrame(update);
 
   const timeDelta = time - lastTime;
   lastTime = time;
 
-  for (let element of compiledElements) {
-    const frame = element.frames.find(frame => frame.time <= time && frame.end >= time);
-    if (!frame && element.dom) {
-      svg.removeChild(element.dom);
-      element.dom = null;
+  for (let animation of animations) {
+    if (animation.end >= time) {
+      const animationTime = time - animation.start;
+      console.log('animating');
+      for (let element of animation.elements) {
+        const frame = element.frames.find(frame => frame.time <= animationTime && frame.end >= animationTime);
+        if (!frame && element.dom) {
+          svg.removeChild(element.dom);
+          element.dom = null;
 
-    } else if (frame) {
-      //console.log(frame.end);
-      if (!element.dom) {
-        element.dom  = element.create(document);
-        svg.appendChild(element.dom);
-      }
+        } else if (frame) {
+          if (!element.dom) {
+            element.dom = element.create(document);
+            svg.appendChild(element.dom);
+          }
 
-      if (frame.update) {
-        const progress = (time - frame.time) / frame.duration;
-        frame.update(element.dom, progress);
+          if (frame.update) {
+            const progress = (animationTime - frame.time) / frame.duration;
+            frame.update(element.dom, progress);
+          }
+        }
       }
+    } else {
+      for (let element of animation.elements) {
+        if (element.dom) {
+          svg.removeChild(element.dom);
+          element.dom = null;
+        }
+      }
+      animations.delete(animation);
     }
   }
 }
 
 requestAnimationFrame(update);
 
-console.log(compiledElements);
 
 function setAttributes(element, attrs) {
   for (let key in attrs) {
